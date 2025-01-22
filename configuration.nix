4b584518ca96612530954149298c1e7b9ac7a8da
Dave Vasilevsky <dave@vasilevsky.ca>
2025-01-22 01:51:29 -0500

    max boot generations

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
     }
 
     { boot.loader.systemd-boot.enable = true;
+      boot.loader.systemd-boot.configurationLimit = 20;
       boot.loader.systemd-boot.xbootldrMountPoint = "/boot";
       boot.loader.efi.efiSysMountPoint = "/efi";
       boot.loader.efi.canTouchEfiVariables = false;
