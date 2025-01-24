c2914cd405bef5443cbf299e9a88ad13d5e5b81d
Dave Vasilevsky <dave@vasilevsky.ca>
2025-01-24 04:47:46 -0500

    touchpad

--- vasi/plasma.nix
+++ vasi/plasma.nix
@@ -14,5 +14,15 @@
         ];
       }
     ];
+
+    input.touchpads = [
+      {
+        name = "SYNA2393:00 06CB:7A13 Touchpad";
+        vendorId = "06cb";
+        productId = "7a13";
+        naturalScroll = true;
+        rightClickMethod = "twoFingers";
+      }
+    ];
   };
 }
