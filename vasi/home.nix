a825a3e3bc7df98aba4900fb09ace8cafe956171
Dave Vasilevsky <dave@vasilevsky.ca>
2025-01-24 03:23:39 -0500

    complete gitconfig

--- vasi/home.nix
+++ vasi/home.nix
@@ -3,5 +3,12 @@
   programs.git = {
     enable = true;
     userName = "Dave Vasilevsky";
+    userEmail = "dave@vasilevsky.ca";
+    aliases = {
+      co = "checkout";
+      st = "status";
+      ci = "commit";
+    };
+    extraConfig.init.defaultBranch = "main";
   };
 }
